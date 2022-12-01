630b981d208fae3a1d4f83174128d6810557abba
E.J. Schroeder <ej.schr@gmail.com>
2022-12-01 00:16:41 -0500

    add day 1

--- src/main/kotlin/lol/schroeder/aoc22/Day01.kt
+++ src/main/kotlin/lol/schroeder/aoc22/Day01.kt
@@ -1,18 +1,21 @@
 package lol.schroeder.aoc22
 
-import lol.schroeder.aoc22.util.readInputToList
+import lol.schroeder.aoc22.util.getResourceAsText
 
-fun part1(input: List<String>) {
+fun part1(input: List<Long>) = input.max()
 
-}
-
-fun part2(input: List<String>) {
-
-}
+fun part2(input: List<Long>) = input
+        .sorted()
+        .takeLast(3)
+        .sum()
 
 fun main() {
-    val input = readInputToList("day1")
+    val input = getResourceAsText("day01")
+        .split("\n\n")
+        .map { it.trim().split("\n").map(String::toLong) }
+        .map { it.sum() }
+
     println("::: Day01 :::")
     println("Part 1: ${part1(input)}")
-//    println("Part 2: ${part2(input)}")
+    println("Part 2: ${part2(input)}")
 }
